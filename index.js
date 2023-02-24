8643991724d162ba0b5b060b86de2605fe4a81ab
Matthew <mbrooks9622@gmail.com>
2023-02-24 15:17:04 +0000

    added function to create manager and engineer

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
-
+const teamMembers = []
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
@@ -22,7 +22,86 @@ function teamMenu(){
                 name: "managerName",
                 message: "What is the team manager's name?"
             },
-        ])
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's Id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the team manager's Office Number?"
+            }
+
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber )
+            teamMembers.push(manager);
+
+        })
         
     }
+    function addEngineer () {
+        inquirer.prompt ([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the Engineer's Name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the Engineer's Id?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the Engineer's Email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the Engineer's GitHub username?"
+            }
+            
+        ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerGithub)
+        teamMembers.push(engineer);
+
+    })
+        
+    }
+
+    function addIntern (){
+
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the Engineer's Name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the Engineer's Id?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the Engineer's Email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the Engineer's GitHub username?"
+            }
+        ])
+    }
+
+
+
 }
